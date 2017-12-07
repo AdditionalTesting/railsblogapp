15451ec2ba8f36978f2c42307a28d4f7526115f5
AlB <albert.burc@gmail.com>
2017-12-07 18:10:05 -0500

    added edit functionality for existing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,20 @@ class ArticlesController < ApplicationController
 
   end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article was updated successfully!"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
+
   def create
       @article = Article.new(article_params)
 
