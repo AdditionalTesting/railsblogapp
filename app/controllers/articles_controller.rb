2acf898060dde867516e51a0f0881426d81b77d3
AlB <albert.burc@gmail.com>
2017-12-07 18:37:07 -0500

    added partials for forms and delete functionality for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,14 @@ end
 
     end
 
+    def destroy
+      @article =Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = 'Article was deleted!'
+      redirect_to articles_path
+
+    end
+
     private
       def article_params
         params.require(:article).permit(:title, :description)
