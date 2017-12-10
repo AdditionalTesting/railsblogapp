2da689c178cbb4a9bbe32a68526650762c4fb843
AlB <albert.burc@gmail.com>
2017-12-09 20:14:54 -0500

    fixed missing end in block

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,8 +66,9 @@ class ArticlesController < ApplicationController
 
   def require_same_user
     if current_user != @article.user
-    flash[:danger] = "You can only edit or delete your own articles."
-    redirect_to root_path
+      flash[:danger] = "You can only edit or delete your own articles."
+      redirect_to root_path
+    end
   end
 
 end
